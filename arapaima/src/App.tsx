a96ef8b41d961618fd107f0e5c66de46ef486f9b
iadev2005 <idaliendres.23@est.ucab.edu.ve>
2025-05-01 23:38:52 -0400

    Agregado el background de las esquinas

--- arapaima/src/App.tsx
+++ arapaima/src/App.tsx
@@ -50,6 +50,43 @@ function Home() {
         </div>
       </div>
 
+      {/* Imágenes debajo de las esquinas */}
+      <div className="absolute top-0 left-0 z-0">
+        <img
+          src="/src/assets/background/sun.svg"
+          alt="Top Left Image"
+          className="w-250 h-250"
+        />
+      </div>
+      <div className="absolute top-0 right-0 z-0">
+        <img
+          src="/src/assets/background/leaf.svg"
+          alt="Top Right Image"
+          className="w-100 h-100"
+        />
+      </div>
+      <div className="absolute top-0 right-0 z-0 mix-blend-saturation">
+        <img
+          src="/src/assets/background/leaf-background.svg"
+          alt="Top Right Background Image"
+          className="w-150 h-150"
+        />
+      </div>
+      <div className="absolute bottom-0 left-0 z-0">
+        <img
+          src="/src/assets/background/fire.svg"
+          alt="Top Right Background Image"
+          className="w-300 h-300"
+        />
+      </div>
+      <div className="absolute bottom-0 right-0 z-0">
+        <img
+          src="/src/assets/background/water.svg"
+          alt="Top Right Background Image"
+          className="w-225 h-125"
+        />
+      </div>
+
       {/* Esquinas decorativas con navegación */}
       <div className="absolute top-0 left-0 z-10" onClick={() => navigate('/page1')}>
         <Border src="/src/assets/icons/border_top_left.svg" alt="Top Left Border" />
