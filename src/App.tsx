1edb16fe18ab97e1e2a1235240c19356ddec4e44
Samuel Guzmán <samisak1805@gmail.com>
2025-05-25 15:39:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { NewDiscussionForm } from './components/forum/NewDiscussionForm';
 import { useAuth } from './context';
 import { getAllPosts, createPost } from './services/forumService';
 import AuthTabs from './components/auth/AuthTabs/AuthTabs';
-import { SuccessMessageProvider, useSuccessMessage } from './context/SuccessMessageContext';
+import { SuccessMessageProvider } from './context/SuccessMessageContext';
 import { Post } from './types/forum';
 
 const categoryLabels: { [key: string]: string } = {
